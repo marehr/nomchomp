e1cfa1c54673af5414963408b5f38e8c02526872
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-03-05 21:54:10 +0100

    [WIP] use new header

--- include/seqan3/argument_parser/validators.hpp
+++ include/seqan3/argument_parser/validators.hpp
@@ -21,6 +21,7 @@
 #include <sstream>
 
 #include <seqan3/argument_parser/auxiliary.hpp>
+#include <seqan3/argument_parser/detail/range_free.hpp>
 #include <seqan3/argument_parser/exceptions.hpp>
 #include <seqan3/core/concept/core_language.hpp>
 #include <seqan3/core/debug_stream/detail/to_string.hpp>
@@ -29,56 +30,6 @@
 #include <seqan3/io/detail/safe_filesystem_entry.hpp>
 #include <seqan3/range/container/concept.hpp>
 #include <seqan3/utility/type_traits/basic.hpp>
-
-namespace seqan3::detail
-{
-
-template <typename value_type>
-std::stringstream & stringify(std::stringstream & stream, value_type && value)
-{
-    stream << value;
-    return stream;
-}
-
-template <typename option_type>
-//!\cond
-    requires named_enumeration<remove_cvref_t<option_type>>
-//!\endcond
-std::stringstream & stringify(std::stringstream & stream, option_type && op)
-{
-    for (auto & [key, value] : enumeration_names<option_type>)
-    {
-        if (op != value)
-            continue;
-
-        stream << key;
-        return stream;
-    }
-
-    stream << "<UNKNOWN_VALUE>";
-    return stream;
-}
-
-template <typename string_range_t>
-std::string join_strings(string_range_t strings, std::string delim)
-{
-    std::ostringstream joined{};
-    std::copy(strings.begin(), strings.end(), std::ostream_iterator<std::string>{joined, delim.c_str()});
-    std::string join = joined.str();
-    if (join.size() >= delim.size()) // remove last delimiter
-        join.erase(join.size() - delim.size());
-    return join;
-}
-
-template <typename ...value_type>
-std::string as_string(value_type && ...values)
-{
-    std::stringstream stream{};
-    (stringify(stream, std::forward<value_type>(values)), ...);
-    return stream.str();
-}
-
-} // namespace seqan3::detail
 
 namespace seqan3
 {
